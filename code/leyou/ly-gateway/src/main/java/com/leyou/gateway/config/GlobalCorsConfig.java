fcf9e34b03e86810cbd632c2dbc147f06aaa0841
cuzz1 <cuzz1234@163.com>
2018-11-03 12:45:13 +0800

    add brand

--- code/leyou/ly-gateway/src/main/java/com/leyou/gateway/config/GlobalCorsConfig.java
+++ code/leyou/ly-gateway/src/main/java/com/leyou/gateway/config/GlobalCorsConfig.java
@@ -21,6 +21,7 @@ public class GlobalCorsConfig {
         //1) 允许的域,不要写*，否则cookie就无法使用了
         config.addAllowedOrigin("http://manage.leyou.com");
         config.addAllowedOrigin("http://api.leyou.com");
+        config.addAllowedOrigin("http://image.leyou.com");
         //2) 是否发送Cookie信息
         config.setAllowCredentials(true);
         //3) 允许的请求方式
